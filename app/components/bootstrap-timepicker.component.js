4560598f5328adaec98b44107f8088e0d34755a3
Jeff Miller <jeffmiller@jeffmiller.name>
2016-05-28 01:08:15 -0400

    Update time picker

--- app/components/bootstrap-timepicker.component.js
+++ app/components/bootstrap-timepicker.component.js
@@ -21,11 +21,11 @@ var BootstrapTimePickerComponent = (function () {
         $element.datetimepicker({
             inline: true,
             sideBySide: true,
-            format: 'HH:mm'
+            format: 'h:mm a'
         });
-        $element.data('DateTimePicker').date(moment(this.time).format('HH:mm'));
+        $element.data('DateTimePicker').date(moment(this.time).format('h:mm a'));
         $element.on('dp.change', function (e) {
-            _this.time = e.date.format('HH:mm');
+            _this.time = e.date.format('h:mm a');
             _this.timeChange.emit(_this.time);
             _this.change.emit(_this.time);
         });
@@ -34,7 +34,7 @@ var BootstrapTimePickerComponent = (function () {
         var $element = $(this.element.nativeElement);
         if ($element.data('DateTimePicker')) {
             if (!this.time) {
-                this.time = moment().format('HH:mm');
+                this.time = moment().format('h:mm a');
                 $element.data('DateTimePicker').date(this.time);
             }
             else {
--- app/components/bootstrap-timepicker.component.ts
+++ app/components/bootstrap-timepicker.component.ts
@@ -20,12 +20,12 @@ export class BootstrapTimePickerComponent implements OnInit, OnChanges {
     $element.datetimepicker({
         inline: true,
         sideBySide: true,
-        format: 'HH:mm'
+        format: 'h:mm a'
     });
-    $element.data('DateTimePicker').date(moment(this.time).format('HH:mm'));
+    $element.data('DateTimePicker').date(moment(this.time).format('h:mm a'));
 
     $element.on('dp.change', (e) => {
-      this.time = e.date.format('HH:mm');
+      this.time = e.date.format('h:mm a');
       this.timeChange.emit(this.time);
       this.change.emit(this.time);
     });
@@ -35,7 +35,7 @@ export class BootstrapTimePickerComponent implements OnInit, OnChanges {
     var $element = $(this.element.nativeElement);
     if($element.data('DateTimePicker')) {
       if(!this.time) {
-        this.time = moment().format('HH:mm');
+        this.time = moment().format('h:mm a');
         $element.data('DateTimePicker').date(this.time);
       } else {
         $element.data('DateTimePicker').date(this.time);
